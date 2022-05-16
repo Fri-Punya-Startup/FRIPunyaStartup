936ed12a408ed96f2d7d0c27f15b85200ccdebda
shalexandeer <shalexandeer@student.telkomuniversity.ac.id>
2022-05-16 12:57:01 +0700

    navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,20 +14,42 @@
     <!-- <link rel="stylesheet" href="/css/navbar/navbar.css"> -->
     <link rel="stylesheet" href="../css/footer/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/navbar/navbar.css">
-
+    
     <style>
-      .content{
+      /* .content{
         margin-top: -80px;
         position: relative;
         z-index: 1;
-      }
+      } */
     </style>
 
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light ">
+  <nav>
+     <div class="parent">
+       <div class="navbar-container">
+        <div class="img-container">
+          <img src="/images/logoNavbar.svg" alt="/" class="responsive">
+        </div>
+        <div class="menu-container" id="mySidenav">
+          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+          <a href="/">Menu</a>
+          <a href="/">About Us</a>
+          <a href="/portofolio">Our Portfolio</a>
+          <a href="/type">Type Portfolio</a>
+          <a href="/">Our Activites</a>
+          <div class="button">
+            <a href="/" class="btnOne">Login</a>
+            <a href="/" class="btnTwo">Contact Us</a>
+          </div>
+        </div>
+        <span class="hamburger" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+        </div>
+     </div>
+  </nav>
+<!-- <nav class="navbar navbar-expand-lg navbar-light ">
   <a class="navbar-brand" href="/">
     <img src="/images/logoNavbar.svg" width="300" height="100" class="d-inline-block align-top" alt="">
   </a>
@@ -63,7 +85,7 @@
     
     </ul>
   </div>
-</nav>
+</nav> -->
 
   <div class="content">
     @yield('content')
@@ -120,7 +142,7 @@
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+  <script src="./js/navbar/navbar.js"></script>
   </body>
 </html>
 
