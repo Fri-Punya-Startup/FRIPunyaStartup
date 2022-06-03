2e1bf1cb7a3070bd5dac5a03385445c357182751
Cicak Kayang <eriscapricron@gmail.com>
2022-06-04 00:05:23 +0700

    Update 2022_05_21_102614_create_events_table.php

--- database/migrations/2022_05_21_102614_create_events_table.php
+++ database/migrations/2022_05_21_102614_create_events_table.php
@@ -21,7 +21,9 @@ return new class extends Migration
             $table->string('khusus');
             $table->string('description');
             $table->string('poster');
-            $table->string('grup-wa');
+            $table->string('grup_wa');
+            $table->string('grup_id')->nullable();
+            $table->string('zoom')->nullable();
             $table->string('tanggal');
             $table->string('waktu');
             $table->timestamps();
