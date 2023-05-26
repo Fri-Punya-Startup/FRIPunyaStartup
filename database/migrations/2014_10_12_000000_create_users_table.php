98bd07571bb05b8e897ba37eaf493bc839f67794
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-05-26 20:14:21 +0700

    migration user, team, dan startup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->foreignId('jabatan_id')->nullable()->default(null);
+            $table->foreignId('team_id')->nullable();
             $table->string('name');
+            $table->string('avatar');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
