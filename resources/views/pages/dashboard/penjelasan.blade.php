7e6d2581fbbaf32de618edaacab7c2de39888a3c
irulhaqq <bemybestfriendruli@gmail.com>
2023-06-27 13:45:54 +0700

    penjelasan ideaforge

---
+++ resources/views/pages/dashboard/penjelasan.blade.php
@@ -0,0 +1,159 @@
+@extends("layouts.main")
+
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Penjelasan Ideaforge</title>
+    <link rel="stylesheet" href="penjelasan.css">
+</head>
+<body>
+    <div class="penjelasan-ideaforge">
+        <div class="penjelasan-idea-forge-penjelasan-idea-forge">
+          <div class="content">
+            <div class="grand-plan">
+              <div class="title-grand-plan">
+                <img
+                  src="images/line3.png"
+                  class="line3"
+                />
+                <span class="text-title">Grand Plan</span>
+              </div>
+              <span class="grand-plan-text">
+                <br />
+                <span>
+                  IdeaForge by FPS adalah wadah bagi para startup enthusiast untuk
+                  menginisiasikan ide startup mereka menuju mvp. IdeaForge ini
+                  merupakan tempat pengasahan ide sebelum masuk fase inkubasi atau
+                  bisa disebut fase ideation.
+                </span>
+                <br />
+                <br />
+                <span>
+                  IdeaForge menyediakan tiga mentor bagi peserta yang ingin
+                  mengajukan pertanyaan seputar ide, sistem, desain, dan lain-lain.
+                  Mentor-mentor ini akan tersedia melalui platform Discord dan akan
+                  sesuai dengan peran mereka sebagai hacker, hustler, dan hipster.
+                  Selama IdeaForge berlangsung, peserta diperbolehkan untuk
+                  mengajukan pertanyaan dan mendapatkan bimbingan bersama mentor
+                  yang telah disediakan.
+                </span>
+                <br />
+                <br />
+                <span>
+                  IdeaForge akan diakhiri dengan para peserta mempresentasikan
+                  produk mereka pada acara demoday atau pitchday, yang akan dinilai
+                  oleh juri yang tersedia.
+                </span>
+              </span>
+            </div>
+            <div class="group1">
+              <div class="frame-proses-teknologi">
+                <div class="title-proses-teknologi">
+                  <img
+                    src="images/line3.png"
+                    class="line3"
+                  />
+                  <span class="text-title">Proses Teknologi</span>
+                </div>
+                <div class="proses-teknologi-row1">
+                  <div class="proses-teknologi-card">
+                    <div class="proses-teknologi-card-description">
+                      <span class="proses-teknologi-text">
+                        <span>
+                          Pengembangan platform dan infrastruktur teknologi yang
+                          dibutuhkan dalam bisnis
+                        </span>
+                      </span>
+                    </div>
+                  </div>
+                  <div class="proses-teknologi-card">
+                    <div class="proses-teknologi-card-description">
+                      <span class="proses-teknologi-text">
+                        <span>
+                          Pemilihan teknologi yang tepat untuk mendukung bisnis yang
+                          dijalankan.
+                        </span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="frame-proses-desain">
+                <div class="title-proses-desain">
+                  <img
+                    src="images/line3.png"
+                    class="line3"
+                  />
+                  <span class="text-title">
+                    <span>Proses Desain</span>
+                  </span>
+                </div>
+                <div class="proses-desain-row">
+                  <div class="proses-desain-card1">
+                    <div class="proses-desain-card-description1">
+                      <span class="proses-desain-text1">
+                        <span>Pengembangan konsep desain yang tepat.</span>
+                      </span>
+                    </div>
+                  </div>
+                  <div class="proses-desain-card2">
+                    <div class="proses-desain-card-description2">
+                      <span class="proses-desain-text2">
+                        Desain produk atau layanan, desain website atau aplikasi,
+                        desain branding, serta pengembangan strategi desain yang
+                        menarik dan efektif
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="frame-output-ideaforge">
+              <div class="output-ideaforge-title">
+                <img
+                  src="images/line3.png"
+                  class="line3"
+                />
+                <span class="text-title">
+                  <span>Output IdeaForge</span>
+                </span>
+              </div>
+              <div class="output-ideaforge-row">
+                <div class="output-ideaforge-card">
+                  <span class="output-ideaforge-text">
+                    <span>Meningkatkan kreativitas dan inovasi</span>
+                  </span>
+                </div>
+                <div class="output-ideaforge-card">
+                  <span class="output-ideaforge-text">
+                    <span>Membuat ide bisnis menjadi produk yang siap</span>
+                  </span>
+                </div>
+                <div class="output-ideaforge-card">
+                  <span class="output-ideaforge-text1">
+                    <span>Meningkatkan pemahaman</span>
+                  </span>
+                </div>
+                <div class="output-ideaforge-card">
+                  <span class="output-ideaforge-text">
+                    <span>Meningkatkan keterampilan dan keahlian</span>
+                  </span>
+                </div>
+                <div class="output-ideaforge-card">
+                  <span class="output-ideaforge-text2">
+                    Meningkatkan jaringan bisnis dan menjalin kemitraan
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
+@endsection
