6787f60e941674528a29cb821379083c171b56d1
NR <nr@Nurul.local>
2023-06-30 21:00:00 +0700

    only register

---
+++ database/migrations/2023_06_26_082220_create_startups_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('startups', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('owners_id');
+            $table->string('startup_name');
+            $table->string('description');
+            $table->string('category');
+            $table->timestamps();
+    
+            $table->foreign('owners_id')->references('id')->on('frsusers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('startups');
+    }
+};
--- public/js/registration.js
+++ public/js/registration.js
@@ -1,40 +1,10 @@
-const hustler = document.getElementById('hustler');
-const hipster = document.getElementById('hipster');
-const hacker = document.getElementById('hacker');
+var checkbox = document.getElementById("check-startup");
+var additionalInputDiv = document.getElementById("additional-input");
 
-const listHustler = document.getElementById('list-hustler');
-const listHipster = document.getElementById('list-hipster');
-const listHacker = document.getElementById('list-hacker');
-
-hacker.addEventListener('click', () => {
-    listHacker.style.display = 'block';    
-    listHipster.style.display = 'none';
-    listHustler.style.display = 'none';
-
-    // listHacker animation display fade
-    listHacker.classList.add('fade-in');
-    listHipster.classList.remove('fade-in');
-    listHustler.classList.remove('fade-in');
-});
-
-hipster.addEventListener('click', () => {
-    listHipster.style.display = 'block';
-    listHacker.style.display = 'none';
-    listHustler.style.display = 'none';
-
-    // listHipster animation display fade
-    listHipster.classList.add('fade-in');
-    listHacker.classList.remove('fade-in');
-    listHustler.classList.remove('fade-in');
-});
-
-hustler.addEventListener('click', () => {
-    listHustler.style.display = 'block';
-    listHipster.style.display = 'none';
-    listHacker.style.display = 'none';
-
-    // listHustler animation display fade
-    listHustler.classList.add('fade-in');
-    listHipster.classList.remove('fade-in');
-    listHacker.classList.remove('fade-in');
-});
+  checkbox.addEventListener("change", function() {
+    if (checkbox.checked) {
+      additionalInputDiv.style.display = "block";
+    } else {
+      additionalInputDiv.style.display = "none";
+    }
+  });
