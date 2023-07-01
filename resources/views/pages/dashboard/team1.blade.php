246c20bd00faadecb2b64c42ff49043b1eba5dff
irulhaqq <bemybestfriendruli@gmail.com>
2023-07-01 17:54:07 +0700

    perbaikan team1

--- resources/views/pages/dashboard/team1.blade.php
+++ resources/views/pages/dashboard/team1.blade.php
@@ -6,156 +6,198 @@
     <link href="./sudahpunyatim.css" rel="stylesheet" />
     <title>Sudah Punya Tim</title>
 </head>
-<body>
-        <div class="sudahpunyatim-container">
-          <div class="sudahpunyatim-sudahpunyatim">
-            <div class="navbar">
-              <span class="sudahpunyatim-text">
-                <span>HOME &gt; IdeaForge &gt; Dashboard &gt; Team &gt;<b>Anggota</b></span>
-              </span>
-              <span class="title-main"><span>TEAM</span></span>
+<div>
+  <div class="punyatim-container">
+    <div class="punyatim-punyatim">
+      <div class="sidebar">
+        <img
+          src="images/back-side.png"
+          class="sidebar-background"
+        />
+        <div class="sidebar-content">
+          <img
+            src="images/ideaforge.png"
+            class="logo-ideaforge"
+          />
+          <div class="sidebar-menu">
+            <div class="sidebar-menu-home">
+              <a href="#">
               <img
-                src="/images/ideaforge.png"
-                class="ideaforge-logo"
-              />
-              <div class="navbar-link">
-                <a href="/home" class="nav-text">HOME</a>
-                <a href="/team" class="nav-text page-at">TEAM</a>
-                <a href="/startup" class="nav-text">STARTUP</a>
-                <a href="/profile" class="nav-text">PROFILE</a>
-              </div>
-            </div>
-            <div class="burger">
+              src="images/sidebar-box.png"
+              class="sidebar-hover-grey"
+            /> </a>
+              <div class="menu-home square">
                 <img
-                  src="/images/Vector.png"
-                  class="vector"
+                  src="images/home.png"
+                  class="icon-home"
                 />
-                <img
-                  src="/images/Vector.png"
-                  class="vector1"
-                />
-                <img
-                  src="/images/Vector.png"
-                  class="vector2"
-                />
-              </div>
-            </div>
-          </div>
-            <span class="nama-tim"><span>Nama Tim</span></span>
-            <div class="frame-ketua">
-              <span class="posisi-ketua"><span>KETUA</span></span>
-              <div class="set-frame">
-                <img
-                  src="/images/ketua.png"
-                  class="set-avatars"
-                />
-            <span class="nama-lengkap"><span>Nama Lengkap</span></span>
-          </div>
-        </div>
-      </div>
-      <div class="group-anggota">
-        <div class="frame-anggota">
-          <div class="set-frame">
-            <div class="set-frame-anggota">
-              <img
-                src="/images/anggota1.png"
-                class="set-avatars"
-              />
-              <span class="nama-lengkap">
-                <span>Nama Lengkap</span>
-              </span>
-            </div>
-          </div>
-          <div class="set-frame">
-            <div class="set-frame-anggota">
-              <img
-                src="/images/anggota2.png"
-                class="set-avatars"
-              />
-              <span class="nama-lengkap">
-                <span>Nama Lengkap</span>
-              </span>
-            </div>
-          </div>
-          <div class="set-frame">
-            <div class="set-frame-anggota">
-              <img
-                src="/images/anggota3.png"
-                class="set-avatars"
-              />
-              <span class="nama-lengkap">
-                <span>Nama Lengkap</span>
-              </span>
-            </div>
-          </div>
-          <div class="set-frame">
-            <div class="set-frame-anggota">
-              <img
-                src="/images/anggota4.png"
-                class="set-avatars"
-              />
-              <span class="nama-lengkap">
-                <span>Nama Lengkap</span>
-              </span>
-            </div>
-          </div>
-        </div>
-        <span class="posisi-anggota"><span>ANGGOTA</span></span>
-      </div>
-      <div class="footer">
-        <div class="frame3">
-          <a href="/ideaforge">
-            <button class="button">
-              <span class="button-text"><span>IdeaForge</span></span> 
-            </button></a>
-            <span class="footer-text-bold">
-              <span>Let&apos;s bring your startup to the next level!!</span></span>
-              <div class="sudahpunyatim-form-group">
-                <span class="footer-text"><span>IdeaForge by FPS</span></span>
-              </div>
-            </div>
-            <div class="footer-frame1"></div>
-            <div class="footer-container1">
-              <div class="footer-row">
-                <img
-                    src="/images/fps.svg"
-                    class="fps-logo"
-                  />
-                  <span class="fps-text-colored"><span>FRI Punya Startup</span></span>
-                  <span class="fps-text-normal"><span>
-                    <span>Bandung,</span><br />
-                    <span>Fakultas Rekayasa Industri</span><br />
-                    <span>Telkom University</span>
-                  </span>
+                <span class="sidebar-text">
+                  <span><a href="#" class="sidebar-text">Home</a></span>
                 </span>
               </div>
-              <div class="social-media">
-                <div class="footer-row1">
-                  <img
-                      src="images/discord.png"
-                      class="social-media-logo"
-                    />
-                    <img
-                      src="images/instagram.png"
-                      class="social-media-logo"
-                    />
-                    <img
-                      src="/images/facebook.png"
-                      class="social-media-logo"
-                    />
-                    <img
-                      src="/images/linked.png"
-                      class="social-media-logo"
-                    />
-                  </div>
-                  <span class="fps-text-normal">
-                    <span>Copyright FPS Telkom, All Right Reserved</span>
+            </div>
+            <div class="sidebar-menu1">
+              <img
+                src="images/hover-sidebar.png"
+                class="sidebar-hover"
+              />
+              <div class="menu-team">
+                <img
+                  src="images/Team.png"
+                  class="icon-team"
+                />
+                <span class="sidebar-text02">
+                  <span><a href="#" class="sidebar-text02">Team</a></span>
+                </span>
+              </div>
+            </div>
+            <div class="sidebar-menu-startup">
+              <a href="#">
+              <img
+              src="images/sidebar-box.png"
+              class="sidebar-hover-grey"
+            /> </a>
+              <div class="menu-startup">
+                <div class="menu-startup-text">
+                  <span class="sidebar-text">
+                    <span><a href="#" class="sidebar-text">Startup</a></span>
+                  </span>
+                </div>
+                <img
+                  src="images/startup.png"
+                  class="icon-startup"
+                />
+              </div>
+            </div>
+            <div class="sidebar-menu-profile">
+              <a href="#">
+              <img
+              src="images/sidebar-box.png"
+              class="sidebar-hover-grey"
+            /> </a>
+              <div class="menu-profile">
+                <img
+                  src="images/profile.png"
+                  class="icon-profile"
+                />
+                <div class="menu-profile-text">
+                  <span class="sidebar-text">
+                    <span><a href="#" class="sidebar-text">Profile</a></span>
                   </span>
                 </div>
               </div>
             </div>
+            <img
+              src="images/end-sidebar.png"
+              class="end-sidebar"
+            />
+            <div class="sidebar-logout">
+              <img
+                src="images/logout.png"
+                class="icon-logout"
+              />
+              <span class="sidebar-text03">
+                <span><a href="#" class="sidebar-text03">Log Out</a></span>
+              </span>
+            </div>
           </div>
         </div>
       </div>
+      <div class="team-content">
+        <div class="profile">
+          <a href="#">
+          <img
+            src="images/profile-pict.png"
+            class="profile-pict"
+          /></a>
+          <span class="username">
+            <span>Bagus Satrio</span>
+          </span>
+          <span class="email">
+            <span>Bagus@gmail.com</span>
+          </span>
+        </div>
+        <span class="team"><span>Team</span></span>
+        <span class="nama-team">
+          <span>Nama Team</span>
+        </span>
+        <div class="frameketua">
+          <div class="frameketua-box">
+            <img
+              src="images/ketua.png"
+              class="profile-pict-team"
+            />
+            <div class="frameketua-identity">
+              <span class="nama">
+                <span>Bagus Satrio</span>
+              </span>
+              <span class="jabatan">
+                <span>Ketua</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="frameanggota">
+          <div class="frameanggota-box">
+            <img
+              src="images/anggota1.png"
+              class="profile-pict-team"
+            />
+            <div class="frameanggota-identity">
+              <span class="nama-anggota">
+                <span>Bagus Satrio</span>
+              </span>
+              <span class="jabatan-anggota">
+                <span>Anggota</span>
+              </span>
+            </div>
+          </div>
+          <div class="frameanggota-box">
+            <img
+              src="images/anggota2.png"
+              class="profile-pict-team"
+            />
+            <div class="frameanggota-identity">
+              <span class="nama-anggota">
+                <span>Bagus Satrio</span>
+              </span>
+              <span class="jabatan-anggota">
+                <span>Anggota</span>
+              </span>
+            </div>
+          </div>
+          <div class="frameanggota-box">
+            <img
+              src="images/anggota3.png"
+              class="profile-pict-team"
+            />
+            <div class="frameanggota-identity">
+              <span class="nama-anggota">
+                <span>Bagus Satrio</span>
+              </span>
+              <span class="jabatan-anggota">
+                <span>Anggota</span>
+              </span>
+            </div>
+          </div>
+          <div class="frameanggota-box">
+            <img
+              src="images/anggota4.png"
+              class="profile-pict-team"
+            />
+            <div class="frameanggota-identity">
+              <span class="nama-anggota">
+                <span>Bagus Satrio</span>
+              </span>
+              <span class="jabatan-anggota">
+                <span>Anggota</span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
